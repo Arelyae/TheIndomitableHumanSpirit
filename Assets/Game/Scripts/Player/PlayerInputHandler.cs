6717003b73377b403d25737920b426122642be0c
Timothée Boiton <timothee.boiton@gmail.com>
2025-01-14 00:17:55 +0100

    Feat : Camera To Fix

--- Assets/Game/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Game/Scripts/Player/PlayerInputHandler.cs
@@ -4,8 +4,15 @@ public class InputHandler : MonoBehaviour
 {
     public Vector3 GetMovementInput()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");  // Left Stick X
+        float vertical = Input.GetAxis("Vertical");      // Left Stick Y
         return new Vector3(horizontal, 0f, vertical).normalized;
+    }
+
+    public Vector2 GetLookInput()
+    {
+        float lookHorizontal = Input.GetAxis("RightStickHorizontal");  // Right Stick X
+        float lookVertical = Input.GetAxis("RightStickVertical");      // Right Stick Y
+        return new Vector2(lookHorizontal, lookVertical);
     }
 }
